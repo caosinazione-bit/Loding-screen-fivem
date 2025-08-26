120071aea8cc3dcc99174f011420ff6d138b192d
caosinazione-bit <caosinazione@gmail.com>
2025-08-26 20:40:57 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Schermata di caricamento per Livorno RP'
+version '1.0.0'
+
+ui_page 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'images/livorno_rp.jpg'
+}
